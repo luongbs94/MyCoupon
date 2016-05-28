1bd7eb90bdbc5fe11876757039a21cbec3b32ce7
nhahv0902 <nhahv0902@gmail.com>
2016-05-28 18:54:05 +0700

    change nav

--- app/src/main/java/com/ln/mycoupon/shop/ShopMainActivity.java
+++ app/src/main/java/com/ln/mycoupon/shop/ShopMainActivity.java
@@ -88,28 +88,36 @@ public class ShopMainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_coupon) {
-            currentPosition = 0;
-            setTitle(getString(R.string.my_coupon));
-            startFragment(new CouponFragment());
-        } else if (id == R.id.nav_new) {
-            currentPosition = 1;
-            setTitle(getString(R.string.news));
-            startFragment(new NewsFragment());
-
-        } else if (id == R.id.nav_history) {
-            currentPosition = 2;
-            setTitle(getString(R.string.history));
-            startFragment(new HistoryFragment());
-
-        } else if (id == R.id.nav_manage) {
-            currentPosition = 2;
-            mFbButton.setVisibility(View.GONE);
-            setTitle(getString(R.string.setting));
-            startFragment(new SettingFragment());
-
-        } else if (id == R.id.logout) {
-            finish();
+        switch (id) {
+            case R.id.nav_coupon:
+                currentPosition = 0;
+                setTitle(getString(R.string.my_coupon));
+                startFragment(new CouponFragment());
+                break;
+            case R.id.nav_new:
+                currentPosition = 1;
+                setTitle(getString(R.string.news));
+                startFragment(new NewsFragment());
+                break;
+            case R.id.nav_history:
+                currentPosition = 2;
+                setTitle(getString(R.string.history));
+                startFragment(new HistoryFragment());
+                break;
+            case R.id.nav_manage:
+                currentPosition = 2;
+                mFbButton.setVisibility(View.GONE);
+                setTitle(getString(R.string.setting));
+                startFragment(new SettingFragment());
+                break;
+            case R.id.logout:
+                finish();
+                break;
+            default:
+                break;
+        }
+        if (id != R.id.nav_manage) {
+            mFbButton.setVisibility(View.VISIBLE);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
