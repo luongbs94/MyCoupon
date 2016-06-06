92f22c54343bc194f9a55e2341686589a3b82684
nhahv0902 <nhahv0902@gmail.com>
2016-06-07 02:26:07 +0700

    add fragment preview images

---
+++ app/src/main/java/com/ln/fragment/PreviewImagesFragment.java
@@ -0,0 +1,30 @@
+package com.ln.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ln.mycoupon.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PreviewImagesFragment extends Fragment {
+
+
+    public PreviewImagesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_preview_images, container, false);
+    }
+
+}
--- app/src/main/java/com/ln/mycoupon/PreviewImagesActivity.java
+++ app/src/main/java/com/ln/mycoupon/PreviewImagesActivity.java
@@ -1,13 +1,42 @@
 package com.ln.mycoupon;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PreviewImagesActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<String> mListImages = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview_images);
+    }
+
+    private class PreviewAdapter extends FragmentPagerAdapter {
+
+        public PreviewAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 }
